b47d5d5a9ad150ce211a3a2ff279b1d50a1caa2b
fuadop <fuadolatunji@gmail.com>
2022-02-14 23:25:03 +0100

    feat: add build script for web frameworks support

--- build.ts
+++ build.ts
@@ -49,6 +49,32 @@ const runBuild = async () => {
     console.log(e);
     process.exit(1)
   });
+
+  // Build to frontend frameworks 
+  build({
+    plugins: [nodeBuiltIns()],
+    entryPoints: ['./src/blockweave.ts'],
+    minify: false,
+    bundle: true,
+    platform: 'browser',
+    target: ['es2019'],
+    format: 'esm', // to allow import support
+    splitting: true,
+    outdir: './web',
+    sourcemap: 'external',
+    define: {
+      'process.env.NODE_DEBUG': 'false',
+      'process.env.NODE_ENV': 'production',
+      'process.env.DEBUG': 'false',
+      'global': 'window',
+      'process.cwd': 'String',
+      '_smartweave_1.LoggerFactory.INST': '{"create": "function() {}"}',
+      '__filename': 'String',
+    }
+  }).catch((e) => {
+    console.log(e);
+    process.exit(1);
+  });
 };
 
 runBuild();
